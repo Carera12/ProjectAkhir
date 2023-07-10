f5eacf9018082981ca21a72dbb188c41d913a2cc
S Chandra <s.chandra.ft21@mail.umy.ac.id>
2023-07-10 18:09:21 +0700

    Menambahkan method FormDataPegawai_Load()

--- ProjectAkhir/Supplier.cs
+++ ProjectAkhir/Supplier.cs
@@ -29,6 +29,24 @@ namespace ProjectAkhir
             this.txtAlmt.DataBindings.Clear();
             this.txtNoTlp.DataBindings.Clear();
         }
+        private void FormDataPegawai_Load()
+        {
+            koneksi.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(new SqlCommand("SELECT ID_supplier, nama_supplier, Alamat_supplier, No_Telepon FROM Supplier", koneksi));
+            DataSet ds = new DataSet();
+            dataAdapter1.Fill(ds);
+
+            this.customersBindingSource.DataSource = ds.Tables[0];
+            this.txtIDSup.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "ID_supplier", true));
+            this.txtNamaSup.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "nama_supplier", true));
+            this.txtAlmt.DataBindings.Add(
+                new Binding("Text", this.customersBindingSource, "Alamat_supplier", true));
+            this.txtNoTlp.DataBindings.Add(
+                 new Binding("Text", this.customersBindingSource, "No_Telepon", true));
+            koneksi.Close();
+        }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
