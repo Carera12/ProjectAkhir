f097f1f038e69bde3eb7789f7fb0d6b72ea01b07
Davit Putra Hartono <davit.putra.ft21@mail.umy.ac.id>
2023-07-12 03:03:47 +0700

    Update UI form Pembelian

--- ProjectAkhir/Pembelian.Designer.cs
+++ ProjectAkhir/Pembelian.Designer.cs
@@ -46,17 +46,23 @@
             this.cmbIDPemb = new System.Windows.Forms.ComboBox();
             this.cmbObat = new System.Windows.Forms.ComboBox();
             this.dtTgl = new System.Windows.Forms.DateTimePicker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DeepPink;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(98, 25);
+            this.dataGridView1.Location = new System.Drawing.Point(98, 96);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(517, 226);
+            this.dataGridView1.Size = new System.Drawing.Size(517, 155);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -65,7 +71,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(360, 336);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.Size = new System.Drawing.Size(56, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "ID Obat";
             // 
@@ -74,7 +80,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(360, 301);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.Size = new System.Drawing.Size(75, 17);
             this.label3.TabIndex = 3;
             this.label3.Text = "ID Pembeli";
             // 
@@ -83,7 +89,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(360, 264);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(126, 16);
+            this.label4.Size = new System.Drawing.Size(130, 17);
             this.label4.TabIndex = 4;
             this.label4.Text = "Tanggal Pembelian";
             // 
@@ -92,7 +98,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(95, 336);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.Size = new System.Drawing.Size(81, 17);
             this.label5.TabIndex = 5;
             this.label5.Text = "Total Bayar";
             // 
@@ -101,7 +107,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(95, 301);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(117, 16);
+            this.label6.Size = new System.Drawing.Size(122, 17);
             this.label6.TabIndex = 6;
             this.label6.Text = "Jumlah pembelian";
             // 
@@ -110,58 +116,73 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(95, 264);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(88, 16);
+            this.label7.Size = new System.Drawing.Size(91, 17);
             this.label7.TabIndex = 7;
             this.label7.Text = "ID Pembelian";
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(642, 200);
+            this.btnClear.BackColor = System.Drawing.Color.DeepPink;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(714, 172);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(54, 36);
+            this.btnClear.Size = new System.Drawing.Size(58, 52);
             this.btnClear.TabIndex = 9;
             this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.UseVisualStyleBackColor = false;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(642, 145);
+            this.btnSave.BackColor = System.Drawing.Color.DeepPink;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(634, 175);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(54, 33);
+            this.btnSave.Size = new System.Drawing.Size(58, 49);
             this.btnSave.TabIndex = 10;
             this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(642, 39);
+            this.btnOpen.BackColor = System.Drawing.Color.DeepPink;
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.ForeColor = System.Drawing.Color.White;
+            this.btnOpen.Location = new System.Drawing.Point(714, 111);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(54, 31);
+            this.btnOpen.Size = new System.Drawing.Size(58, 47);
             this.btnOpen.TabIndex = 11;
             this.btnOpen.Text = "Open";
-            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.UseVisualStyleBackColor = false;
             this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(642, 96);
+            this.btnAdd.BackColor = System.Drawing.Color.DeepPink;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(634, 111);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(54, 29);
+            this.btnAdd.Size = new System.Drawing.Size(58, 45);
             this.btnAdd.TabIndex = 12;
             this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnClose
             // 
+            this.btnClose.BackColor = System.Drawing.Color.DeepPink;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.Location = new System.Drawing.Point(17, 403);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(64, 35);
             this.btnClose.TabIndex = 13;
             this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // txtTotBay
@@ -190,7 +211,7 @@
             this.cmbIDPemb.FormattingEnabled = true;
             this.cmbIDPemb.Location = new System.Drawing.Point(494, 298);
             this.cmbIDPemb.Name = "cmbIDPemb";
-            this.cmbIDPemb.Size = new System.Drawing.Size(121, 24);
+            this.cmbIDPemb.Size = new System.Drawing.Size(202, 24);
             this.cmbIDPemb.TabIndex = 18;
             // 
             // cmbObat
@@ -198,7 +219,7 @@
             this.cmbObat.FormattingEnabled = true;
             this.cmbObat.Location = new System.Drawing.Point(494, 333);
             this.cmbObat.Name = "cmbObat";
-            this.cmbObat.Size = new System.Drawing.Size(121, 24);
+            this.cmbObat.Size = new System.Drawing.Size(198, 24);
             this.cmbObat.TabIndex = 19;
             // 
             // dtTgl
@@ -208,11 +229,44 @@
             this.dtTgl.Size = new System.Drawing.Size(200, 22);
             this.dtTgl.TabIndex = 20;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepPink;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(805, 70);
+            this.panel1.TabIndex = 21;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjectAkhir.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(-91, -36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1012, 580);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DeepPink;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(34, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Pembelian";
+            // 
             // Pembelian
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dtTgl);
             this.Controls.Add(this.cmbObat);
             this.Controls.Add(this.cmbIDPemb);
@@ -231,10 +285,14 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Pembelian";
             this.Text = "Pembelian";
             this.Load += new System.EventHandler(this.Pembelian_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -260,5 +318,8 @@
         private System.Windows.Forms.ComboBox cmbIDPemb;
         private System.Windows.Forms.ComboBox cmbObat;
         private System.Windows.Forms.DateTimePicker dtTgl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
